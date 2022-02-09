907e3b128ebd96e7fb9c0862355f0348fae68a10
unknown <arjunawira26@gmail.com>
2022-02-10 01:23:40 +0700

    Fourth commit halaman Cart dan Success

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -8,7 +8,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="breadcrumb-text product-more text-left">
-                        <a href="./home.html"><i class="fa fa-home"></i> Home</a>
+                        <router-link to="/"><i class="fa fa-home"></i> Home</router-link>
                         <span>Detail</span>
                     </div>
                 </div>
@@ -30,19 +30,20 @@
                             <div class="product-thumbs">
                                 <carousel class="product-thumbs-track ps-slider" :items="3" :nav="false">
 
-                                    <div class="pt active" data-imgbigurl="img/mickey1.jpg">
+                                    <!-- <div class="pt active" data-imgbigurl="img/mickey1.jpg"> -->
+                                    <div class="pt" @click="changeImage(thumbs[0])" :class="thumbs[0] == default_product_picture ? 'active' : '' ">
                                         <img src="img/mickey1.jpg" alt="" />
                                     </div>
 
-                                    <div class="pt" data-imgbigurl="img/mickey2.jpg">
+                                    <div class="pt" @click="changeImage(thumbs[1])" :class="thumbs[1] == default_product_picture ? 'active' : '' ">
                                         <img src="img/mickey2.jpg" alt="" />
                                     </div>
 
-                                    <div class="pt" data-imgbigurl="img/mickey3.jpg">
+                                    <div class="pt" @click="changeImage(thumbs[2])" :class="thumbs[2] == default_product_picture ? 'active' : '' ">
                                         <img src="img/mickey3.jpg" alt="" />
                                     </div>
 
-                                    <div class="pt" data-imgbigurl="img/mickey4.jpg">
+                                    <div class="pt" @click="changeImage(thumbs[3])" :class="thumbs[3] == default_product_picture ? 'active' : '' ">
                                         <img src="img/mickey4.jpg" alt="" />
                                     </div>
 
@@ -68,7 +69,7 @@
                                     <h4>$495.00</h4>
                                 </div>
                                 <div class="quantity">
-                                    <a href="shopping-cart.html" class="primary-btn pd-cart">Add To Cart</a>
+                                    <router-link to="/cart" class="primary-btn pd-cart">Add To Cart</router-link>
                                 </div>
                             </div>
                         </div>
@@ -79,13 +80,14 @@
     </section>
     <!-- Product Shop Section End -->
 
+    <RelatedShayna />
     <FooterShayna />
   </div>
 </template>
 
 <script>
 import HeaderShayna from "../components/HeaderShayna.vue";
-
+import RelatedShayna from "../components/RelatedShayna.vue"
 import FooterShayna from "../components/FooterShayna.vue";
 
 import carousel from 'vue-owl-carousel';
@@ -94,14 +96,24 @@ export default {
     name: "product",
     components: {
     HeaderShayna,
-
+    RelatedShayna,
     FooterShayna,
-
     carousel,
   },
   data() {
       return{
-          default_product_picture: "img/mickey1.jpg"
+          default_product_picture: "img/mickey1.jpg",
+          thumbs: [
+              "img/mickey1.jpg",
+              "img/mickey2.jpg",
+              "img/mickey3.jpg",
+              "img/mickey4.jpg",
+          ]
+      }
+  },
+  methods: {
+      changeImage(urlImage){
+          this.default_product_picture = urlImage;
       }
   }
 }
